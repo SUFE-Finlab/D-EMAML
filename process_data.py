ecfbd35394b5202ded7989cc24b54d720770e14d
SUFE-Finlab <finlab@fin-lab.ai>
2025-11-13 15:19:07 +0800

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -6,9 +6,26 @@ from torch_geometric.data import Data
 import pandas as pd
 from tqdm import tqdm
 from torch_geometric.transforms import RemoveIsolatedNodes
+from utils import DataSplit
 
 
-def processed_data(raw_table, save_path_train, save_path_test):
+trans = DataSplit()
+def initialize_bank_indices(data, save_path):
+    print(f'initializing for file {save_path}')
+    lis_dic = {}
+    size_dic = {}
+    bank_index = data.bank_index.to('cuda:0')
+    banks = bank_index.unique()
+    for bank in tqdm(banks):
+        bank = bank.cpu().item()
+        from_is = bank_index[0]==bank
+        to_is = bank_index[1]==bank
+        temp = torch.where((from_is|to_is))[0]
+        lis_dic[bank] = temp.cpu()
+        size_dic[bank] = temp.shape[0]
+    torch.save((lis_dic, size_dic), save_path)
+
+def processed_data(raw_table, save_path):
     print('Reading Data...')
     try:
         data = pd.read_csv(raw_table)
@@ -56,49 +73,60 @@ def processed_data(raw_table, save_path_train, save_path_test):
     trans_data['to_id'] = to_id['id']
     print('Constructing PT file...')
     data = Data()
-    # data.bank_index = torch.tensor(trans_data[['From Bank','To Bank']].values,dtype = torch.int64)
+    data.bank_index = torch.tensor(trans_data[['From Bank','To Bank']].values,dtype = torch.int64)
     data.edge_index = torch.tensor(trans_data[['from_id','to_id']].values,dtype = torch.int64)
     data.time = torch.tensor(trans_data['Unix_Timestamp'].values,dtype = torch.int64)
     data.y = torch.tensor(trans_data['Is Laundering'],dtype = torch.int64)
     data.edge_attr = torch.tensor(trans_data.drop(columns = ['Timestamp','Is Laundering','From','To','Unix_Timestamp','from_id','to_id','From Bank','To Bank']).values.astype(np.float32),dtype = torch.float32)
-    data.x = torch.randn((len(unique_data),8),dtype = torch.float32)
-    # data.bank_index = data.bank_index.transpose(0,1)
+    data.x = torch.ones((len(unique_data),1),dtype = torch.float32)
+    data.bank_index = data.bank_index.transpose(0,1)
     data.edge_index = data.edge_index.transpose(0,1)
-    print(f'Spliting for data {save_path_train}...')
-    data = data.sort_by_time()
-    train_size = int(data.num_edges*0.7)
-    test_size = int(data.num_edges*0.3)
-
-    transform = RemoveIsolatedNodes()
-    train_data = Data()
-    train_data.edge_index = data.edge_index[:,:train_size]
-    train_data.time = data.time[:train_size]
-    train_data.y = data.y[:train_size]
-    train_data.edge_attr = data.edge_attr[:train_size]
-    train_data.x = data.x
-    train_data = transform(train_data)
-    # train_data
-    test_data = Data()
-    test_data.edge_index = data.edge_index[:,-test_size:]
-    test_data.time = data.time[-test_size:]
-    test_data.y = data.y[-test_size:]
-    test_data.edge_attr = data.edge_attr[-test_size:]
-    test_data.x = data.x
-    test_data = transform(test_data)
-    # test_data
-    print(f'Saving for data {save_path_train}')
-    torch.save(data,save_path_train)
-    print(f'Saving for data {save_path_test}')
-    torch.save(data,save_path_test)
+    print(f'Validation for data {save_path}:{data.validate()}')
+    train_data, valid_data, test_data = trans(data)
+    torch.save(train_data,f'{save_path}/train.pt')
+    torch.save(valid_data,f'{save_path}/valid.pt')
+    torch.save(test_data,f'{save_path}/test.pt')
+    torch.save(data,f'{save_path}/raw_data.pt')
     return
-if __name__=="__main__":
-    raw_path = './data/raw'
-    processed_path = './data/processed'
-    name = 'HI-Small'
-    csv_name = 'HI-Small_Trans'
-    csv_path = f'{raw_path}/{csv_name}.csv'
-    pt_train_path = f'{processed_path}/{name}_train.pt'
-    pt_test_path = f'{processed_path}/{name}_test.pt'
-    print(f"Processing {csv_name}...")
-    processed_data(csv_path, pt_train_path, pt_test_path)
+def AML_Proceeding():
+    raw_path = './data/AMLWorld/raw'
+    processed_path = './data/AMLWorld/processed'
+    lis_name = ['HI-Small_Trans']
+    for name in lis_name:
+        csv_path = f'{raw_path}/{name}.csv'
+        pt_path = f'{processed_path}'
+        print(f"Processing {name}...")
+        processed_data(csv_path, pt_path)
+    
     print("Path to dataset files:", processed_path)
+
+def DGraph_Proceeding():
+    raw_path = './data/DGraph-Fin/raw'
+    processed_path = './data/DGraph-Fin/processed'
+    name = 'dgraphfin.npz'
+    npz_path = f'{raw_path}/{name}'
+    save_path = f"{processed_path}"
+    data = np.load(npz_path)
+    g = Data()
+    g.x = torch.tensor(data['x'], dtype = torch.float32)
+    y = torch.tensor(data['y'], dtype = torch.int64)
+    g.edge_index = torch.tensor(data['edge_index'], dtype = torch.int64).transpose(0,1)
+    g.edge_type = torch.tensor(data['edge_type'], dtype = torch.int64)
+    g.edge_attr = torch.zeros((g.edge_index.shape[1],1),dtype = torch.float32)
+    g.time = torch.tensor(data['edge_timestamp'], dtype = torch.int64)
+    g.y = torch.zeros(g.edge_index.shape[1], dtype = torch.int64)
+    node_type = y
+    node_type[node_type == 1] = 0
+    node_type[node_type == 2] = 1
+    node_type[node_type == 3] = 2
+    g.node_type = node_type
+    g.y[(y[g.edge_index[0]]==1)&(y[g.edge_index[1]]==1)] = 1
+    print(f'Validation for data {save_path}:{g.validate()}')
+    train_data, valid_data, test_data = trans(g)
+    torch.save(train_data,f'{save_path}/train.pt')
+    torch.save(valid_data,f'{save_path}/valid.pt')
+    torch.save(test_data,f'{save_path}/test.pt')
+    torch.save(g,f'{save_path}/raw_data.pt')
+if __name__=="__main__":
+    AML_Proceeding()
+    DGraph_Proceeding()
